657c4e99b20f678faf9ec1acd41f11946cb1d51d
rizkyapri <rizkyapriansyah625@gmail.com>
2025-02-27 11:55:21 +0700

    fix: footer

--- resources/views/inc/_footer.blade.php
+++ resources/views/inc/_footer.blade.php
@@ -4,13 +4,13 @@
             {{ $profileApp->app_pengembang ?? '' }}</span>
     </div>
     <div>
-        <ul class="list-table m-0">
+        {{-- <ul class="list-table m-0">
             <li><a href="{{ route('introduction') }}" class="text-secondary fw-700">About</a></li>
             <li class="pl-3"><a href="{{ route('licensing') }}" class="text-secondary fw-700">License</a></li>
             <li class="pl-3"><a href="{{ route('general') }}" class="text-secondary fw-700">Documentation</a>
             </li>
             <li class="pl-3 fs-xl"><a href="https://wrapbootstrap.com/user/MyOrange" class="text-secondary"
                     target="_blank"><i class="fal fa-question-circle" aria-hidden="true"></i></a></li>
-        </ul>
+        </ul> --}}
     </div>
 </footer>
